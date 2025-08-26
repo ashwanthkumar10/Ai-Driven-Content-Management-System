ccd74466f1817330bb1e92796d667de4e3b0292c
Ashwanth Kumar <Ashwanth.Kumar@valuehealthai.com>
2025-08-26 23:24:01 +0530

    Auth middleware is added for the security

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', userController.login);
-router.post('/create-user', userController.createUser);
-router.put('/edit-user/:id', userController.editUser);
-router.get('/get-all-users', userController.getAllUsers);
+router.post('/create-user',authMiddleware ,  userController.createUser);
+
+
+router.put('/edit-user/:id', authMiddleware,  userController.editUser);
+router.get('/get-all-users',authMiddleware,  userController.getAllUsers);
 
 export default router;
